5eb76c5767cc62bca7e825c8ceae92b38e58ba7d
inslog94 <inslog94@gmail.com>
2023-08-21 23:13:12 +0900

    ✨ 소셜 로그인 기능 추가(Google)

--- everyplace/user/urls.py
+++ everyplace/user/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/', include('allauth.urls')),
     
+    path('auth/google/login/', views.google_login, name='google_login'),
+    path('auth/google/callback/', views.google_callback, name='google_callback'),
+    path('auth/google/login/finish/', views.GoogleLogin.as_view(), name='google_login_todjango'),
 ]
