7606ad631ca0f8c6c649ea67e8233bcb17013168
young <cocoujy@naver.com>
2023-08-22 14:40:07 +0900

    ✨ board 수정 기능 추가

--- everyplace/board/views.py
+++ everyplace/board/views.py
@@ -6,22 +6,24 @@ from .serializers import BoardSerializer, BoardTagSerializer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import authentication_classes, permission_classes
+from django.shortcuts import get_object_or_404
 
 
 class BoardView(APIView):
-    ## 보드 전체 목록 조희
+    ## 특정 보드 상세 조회 메소드
     def get_object(self, pk):
         try:
             return Board.objects.get(pk=pk, is_deleted=False)
         except Board.DoesNotExist:
             return None
 
-    ## 특정 보드 상세 조회
     def get(self, request, pk=None):
+        ## 보드 전체 목록 조희
         if not pk:
             boards = Board.objects.filter(is_deleted=False)
             serializer = BoardSerializer(boards, many=True)
             return Response(serializer.data)
+        ## 특정 보드 상세 조회
         else:
             board = self.get_object(pk)
             if board is None:
@@ -57,5 +59,36 @@ class BoardView(APIView):
                     board.tags.add(tag)
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    ## 보드 수정
+    def put(self, request, pk):
+        board = get_object_or_404(Board, id=pk)
+        data = request.data.copy()
+        tags_data = None
+        if 'tags' in data:
+            tags_data = data.pop('tags')
+
+        serializer = BoardSerializer(board, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+
+            if tags_data:
+                # 기존 태그 연결을 삭제
+                board.tags.clear()
+                for tag_data in tags_data:
+                    try:
+                        tag = BoardTag.objects.get(content=tag_data)
+                    except BoardTag.DoesNotExist:
+                        tag_serializer = BoardTagSerializer(data={'content': tag_data})
+                        if tag_serializer.is_valid():
+                            tag = tag_serializer.save()
+                        else:
+                            return Response(tag_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                    # 새로운 태그를 연결
+                    board.tags.add(tag)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
