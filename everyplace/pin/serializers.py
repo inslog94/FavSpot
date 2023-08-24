3bec521dc443f65afd8d18df91ba8e3c9c43e5cd
young <cocoujy@naver.com>
2023-08-24 21:38:42 +0900

    🩹 SimplePinSerializer의 필터링 작업 코드 수정

--- everyplace/pin/serializers.py
+++ everyplace/pin/serializers.py
@@ -20,7 +20,7 @@ class SimplePinSerializer(serializers.ModelSerializer):
     def get_pincontent_set(self, pin):
         # 하나의 핀 객체에 연결되어있는 핀컨텐츠 객체들 추출 (역참조) 
         # photo 필드가 있는 것들을 최신순으로 3개만 추출
-        contents = pin.pincontent_set.filter(photo__isnull=False).order_by('-created_at')[:3]
+        contents = pin.pincontent_set.order_by('-created_at')[:3]
         photo_urls = [content.photo.url for content in contents if content.photo]
         return photo_urls
     
