dab9ef5288bd2f0a6f925a75b2400391bda4e807
bloodsteelrain <chwoo9528@gmail.com>
2023-09-13 11:22:02 +0900

    ✨ 보드 댓글 생성시 알림을 생성하고 웹소켓 연결을 통해 보드 주인에게 알리는 기능 구현

--- everyplace/board/consumers.py
+++ everyplace/board/consumers.py
@@ -1,6 +1,12 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from board.models import BoardComment
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -34,3 +40,25 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             'message': message,
         }))
+
+
+# BoardComment가 생성되었을때 실행되는 데코레이터
+@receiver(post_save, sender=BoardComment)
+def send_notification_to_owner(sender, instance, created, **kwargs):
+    # 생성 트리거 확인
+    if created:
+        # 댓글 단 보드의 주인 유저 확인
+        board_owner_id = instance.board_id.user_id.id
+        # 댓글 단 보드의 주인과 댓글 단 유저가 다를 시 신호 보냄
+        if board_owner_id != instance.user_id.id:
+            channel_layer = get_channel_layer()
+            message = f"'{instance.board_id.title}'에 새 댓글이 달렸습니다"
+
+            # WebSocket 연결을 통해 보드 주인에게 알림
+            async_to_sync(channel_layer.group_send)(
+                f'notification_{board_owner_id}',
+                {
+                    "type": "send.notification",
+                    "message": message,
+                }
+            )
