7a0536b6e2d3bc50ad4f1203c6f1682ab3ffc07e
inslog94 <inslog94@gmail.com>
2023-09-27 13:57:18 +0900

    ✨ Middleware를 통해 인증 관련 status code 처리

--- everyplace/app/middleware.py
+++ everyplace/app/middleware.py
@@ -1,4 +1,3 @@
-from rest_framework.exceptions import PermissionDenied
 from rest_framework import status
 from django.http import JsonResponse
 
@@ -13,26 +12,13 @@ class CookieToAuthorizationMiddleware:
             request.META['HTTP_AUTHORIZATION'] = f'Bearer {token}'
 
         response = self.get_response(request)
-        return response
-    
-
-class Convert403to401Middleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-
-        if response.status_code == 403:
-            try:
-                detail = response.data.get('detail', '')
-
-                if 'authentication' in detail.lower():
-                    response.status_code = 401
-            except AttributeError:
-                pass
-
-        if response.status_code == 401:
-            response = JsonResponse({'error': '로그인하지 않은 사용자는 이용할 수 없습니다.'}, status=status.HTTP_401_UNAUTHORIZED)
-
+        # 인증되지 않은 경우 rest_framework.permissions.IsAuthenticated로 인해 403(Forbidden) 응답 반환
+        if response.get('data', ''):
+            token_not_given = "authentication" in response.data.get('detail', '') # 요청에 JWT 토큰이 포함되지 않은 경우
+            token_not_valid = "not valid" in response.data.get('detail', '') # 만료된 JWT 토큰이 전달된 경우
+            
+        if response.status_code == 403 and (token_not_given or token_not_valid):
+            response = JsonResponse({'err_msg': '로그인이 필요합니다.'}, status=status.HTTP_401_UNAUTHORIZED) # 401(Unauthorized) 응답으로 바꿔서 반환
+            response.set_cookie('login_check', False) # Cookies에 로그인 여부 False값 담아서 응답
+            
         return response
