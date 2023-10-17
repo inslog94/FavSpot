2d2e00c0f712deefecf6af271176baa66ee09b98
young <cocoujy@naver.com>
2023-10-17 11:46:16 +0900

    🔧 CookieToAuthorizationMiddleware 미들웨어 코드 수정

--- everyplace/app/middleware.py
+++ everyplace/app/middleware.py
@@ -17,8 +17,8 @@ class CookieToAuthorizationMiddleware:
             token_not_given = "authentication" in response.data.get('detail', '') # 요청에 JWT 토큰이 포함되지 않은 경우
             token_not_valid = "not valid" in response.data.get('detail', '') # 만료된 JWT 토큰이 전달된 경우
             
-        if response.status_code == 403 and (token_not_given or token_not_valid):
-            response = JsonResponse({'error': '로그인하지 않은 사용자는 이용할 수 없습니다.'}, status=status.HTTP_401_UNAUTHORIZED) # 401(Unauthorized) 응답으로 바꿔서 반환
-            response.set_cookie('login_check', False) # Cookies에 로그인 여부 False값 담아서 응답
+            if response.status_code == 403 and (token_not_given or token_not_valid):
+                response = JsonResponse({'error': '로그인하지 않은 사용자는 이용할 수 없습니다.'}, status=status.HTTP_401_UNAUTHORIZED) # 401(Unauthorized) 응답으로 바꿔서 반환
+                response.set_cookie('login_check', False) # Cookies에 로그인 여부 False값 담아서 응답
             
         return response
