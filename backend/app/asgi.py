f05b2a0e2249da786a139b96ef1dfac6a8c5a5f8
inslog94 <inslog94@gmail.com>
2023-10-19 15:53:29 +0900

    🔧 배포 시 사용되는 asgi 관련 환경설정

--- backend/app/asgi.py
+++ backend/app/asgi.py
@@ -8,11 +8,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from notification.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = ProtocolTypeRouter(
     {
