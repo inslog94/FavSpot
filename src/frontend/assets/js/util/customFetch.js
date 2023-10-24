165fd9b63b99ec581977af1da140e35d2423897e
inslog94 <inslog94@gmail.com>
2023-10-24 11:10:42 +0900

    🩹 토큰 만료 or 잘못된 토큰 전달 시 인증 초기화

--- src/frontend/assets/js/util/customFetch.js
+++ src/frontend/assets/js/util/customFetch.js
@@ -1,7 +1,14 @@
 export async function customFetch(url, options) {
   const response = await fetch(url, options);
+  const currentPathname = window.location.pathname;
 
-  if (response.status === 401) {
+  if (
+    !(
+      currentPathname == '/assets/html/board_detail.html#' ||
+      currentPathname == '/assets/html/board_detail.html'
+    ) &&
+    (response.status === 401 || response.status === 403)
+  ) {
     alert('원활한 서비스 이용을 위해 로그인이 필요합니다.');
     window.location.href = '/assets/html/login.html';
   } else if (!response.ok) {
